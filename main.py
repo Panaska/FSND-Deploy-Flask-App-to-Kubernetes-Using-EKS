37d01022b60742734d635bcab516c2eba5734b64
Elena Zelenskaya <elena.a.zelenskaya@gmail.com>
2019-11-27 04:53:38 -0800

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthier!")
+    return jsonify("Healthier!!")
 
 
 @APP.route('/auth', methods=['POST'])
